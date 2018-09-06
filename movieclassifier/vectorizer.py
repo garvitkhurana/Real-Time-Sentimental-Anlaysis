4159c0e00a6f14ae3b443498b2faf7f77b117633
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-06 23:39:45 +0530

    adding final files

--- movieclassifier/vectorizer.py
+++ movieclassifier/vectorizer.py
@@ -3,7 +3,7 @@ import re
 import os
 import pickle
 
-cur_dir = os.path.dirname(os.path.abspath('__file__'))
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 stop = pickle.load(open(os.path.join(cur_dir, 'pkl_objects','stopwords.pkl'), 'rb'))
 
 def tokenizer(text):
