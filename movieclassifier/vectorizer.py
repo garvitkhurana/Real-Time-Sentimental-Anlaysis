3d218760fd4d12ebf5744da23c0ffe560ba06be8
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-06 09:21:28 +0530

    modifying db

--- movieclassifier/vectorizer.py
+++ movieclassifier/vectorizer.py
@@ -34,7 +34,32 @@ vect = HashingVectorizer(decode_error='ignore', n_features=2**21,preprocessor=No
 
 # print(label[clf.predict(X)[0]],np.max(clf.predict_proba(X))*100)
 
+
+
 conn = sqlite3.connect('reviews.sqlite')
-print(conn)
 c = conn.cursor()
+'''
+c.execute('CREATE TABLE review_db'\
+        ' (review TEXT, sentiment INTEGER, date TEXT)')
+
+example1 = 'I love this movie'
+c.execute("INSERT INTO review_db"\
+        " (review, sentiment, date) VALUES"\
+        " (?, ?, DATETIME('now'))", (example1, 1))
+
+example2 = 'I disliked this movie'
+c.execute("INSERT INTO review_db"\
+        " (review, sentiment, date) VALUES"\
+        " (?, ?, DATETIME('now'))", (example2, 0))
+
+conn.commit()
 conn.close()
+'''
+
+conn = sqlite3.connect('reviews.sqlite')
+c = conn.cursor()
+c.execute("SELECT * FROM review_db WHERE date"\
+        " BETWEEN '2016-11-01 00:00:00' AND DATETIME('now')")
+results = c.fetchall()
+conn.close()
+results
