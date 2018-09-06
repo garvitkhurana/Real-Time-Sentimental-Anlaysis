6674127140e74a5704bb19d3c6082543683b64b1
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-06 09:10:34 +0530

    working with sql

--- movieclassifier/vectorizer.py
+++ movieclassifier/vectorizer.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import HashingVectorizer
 import re
 import os
 import pickle
+import sqlite3
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 stop = pickle.load(open(os.path.join(cur_dir, 'pkl_objects','stopwords.pkl'), 'rb'))
@@ -13,7 +14,27 @@ def tokenizer(text):
     tokenized = [w for w in text.split() if w not in stop]
     return tokenized
 
+#Using HashingVectorizer to convert a collection of text documents to a matrix of token occurrences
 vect = HashingVectorizer(decode_error='ignore', n_features=2**21,preprocessor=None, tokenizer=tokenizer)
 
-clf=pickle.load(open(os.path.join(cur_dir, 'pkl_objects','classifier.pkl'), 'rb'))
-print(clf)
+# clf=pickle.load(open(os.path.join(cur_dir, 'pkl_objects','classifier.pkl'), 'rb'))
+
+
+# import numpy as np 
+# label={0:"negative",1:"positive"}
+
+# sample=["The actor sucks but the movie was too good"]
+# X=vect.transform(sample)
+
+# print(clf.predict(X))
+
+# print(clf.predict(X)[0])
+
+# print(clf.predict_proba(X))
+
+# print(label[clf.predict(X)[0]],np.max(clf.predict_proba(X))*100)
+
+conn = sqlite3.connect('reviews.sqlite')
+print(conn)
+c = conn.cursor()
+conn.close()
